34c937ca7d57881d0c0c80d17c3c4ad1235e4a5e
Sochukwuma Nwokoye <somanwokoye@gmail.com>
2020-09-13 13:35:27 +0100

    created items service

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put , Delete, Body, Param } from '@nestjs/common';
+import {CreateItemDto} from './dto/create-item.dto';
 
 @Controller('items')
-export class ItemsController {}
+export class ItemsController {
+    @Get()
+    finadall(): string {
+        return `Get all items `;
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id) : string {
+        return `Item ${id}`;
+    }
+
+    @Post()
+    create(@Body() createItemDto : CreateItemDto): string {
+        return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id): string {
+        return `Delete ${id}`;
+    }
+
+    @Put(':id')
+    update(@Body() updateItemDto : CreateItemDto, @Param('id') id){
+        return `Update ${id} - Name: ${updateItemDto.name}`
+    }
+
+
+}
