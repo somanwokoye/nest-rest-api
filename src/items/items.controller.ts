a51471ff1a095b3090909b6a6764c337c0c8e435
Sochukwuma Nwokoye <somanwokoye@gmail.com>
2020-09-13 13:53:55 +0100

    Add item-service with findAll() and findOne() functions

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,16 +1,20 @@
 import { Controller, Get, Post, Put , Delete, Body, Param } from '@nestjs/common';
 import {CreateItemDto} from './dto/create-item.dto';
+import {ItemsService} from './items.service';
+import {Item} from './interfaces/item.interface'
 
 @Controller('items')
 export class ItemsController {
+    constructor(private readonly itemsService : ItemsService) {}
+
     @Get()
-    finadall(): string {
-        return `Get all items `;
+    finadall(): Item[] {
+        return this.itemsService.findAll();
     }
 
     @Get(':id')
-    findOne(@Param('id') id) : string {
-        return `Item ${id}`;
+    findOne(@Param('id') id) : Item {
+        return this.itemsService.findOne(id);
     }
 
     @Post()
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import {Item} from './interfaces/item.interface';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+    private readonly items: Item[] =[
+        {
+            id : "34484892093",
+            name : "Item One",
+            qty : 100,
+            description : "This is Item One"
+        },
+
+        {
+            id : "454554545454",
+            name : "Item Two",
+            qty : 210,
+            description : "This is Item Two"
+        }
+    ];
+
+    findAll(): Item[] {
+        return this.items;
+    }
+
+    findOne(id : string): Item {
+        return this.items.find(item => item.id === id);
+    }
+}
